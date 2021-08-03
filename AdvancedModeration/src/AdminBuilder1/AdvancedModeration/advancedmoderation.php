f3b688cf82a52dd3f6c3c9f56396283e659b12d5
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-08-03 07:16:27 -0500

    Add files via upload

--- AdvancedModeration/src/AdminBuilder1/AdvancedModeration/advancedmoderation.php
+++ AdvancedModeration/src/AdminBuilder1/AdvancedModeration/advancedmoderation.php
@@ -12,7 +12,7 @@ use pocketmine\command\CommandSender;
 
 use pocketmine\event\Listener;
 
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as C;
 # commands
 use AdminBuilder1\AdvancedModeration\command\help;
 
@@ -25,10 +25,12 @@ class AdvancedModeration extends PluginBase implements Listener {
 	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getLogger()->info("AdvancedModerator is enabled");
+		$this->getLogger()->info(C::GREEN . "AdvancedModerator is enabled");
+		$this->saveDefaultConfig(); // Saves config.yml if not created.
+		$this->reloadConfig(); // Fix bugs sometimes by getting configs values
 	}
 	public function onDisabled(){
-		$this->getLogger()->info("AdvancedModerator is disabled");
+		$this->getLogger()->info(C::RED . "AdvancedModerator is disabled");
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
@@ -37,10 +39,11 @@ class AdvancedModeration extends PluginBase implements Listener {
 			 # cmd
 			case "advancedmod":
 			case "advmod":
-					# default
+//if(){
+			# default
 			if($sender instanceof Player){
 				if(!isset($args[0])){
-					 if (!$sender->hasPermission("advancedmod.cmd")) {
+					 if (!$sender->hasPermission("advancedmod.command")) {
                             $sender->sendMessage($this->noperm);
                             return true;
                         } else {
@@ -49,7 +52,35 @@ class AdvancedModeration extends PluginBase implements Listener {
                         }
 				
 				}
+				# help cmd
+				if(count($args) == 1 && $args[0] === "help"){
+					if(!$sender->hasPermission("advancedmod.help")){
+					$sender->sendMessage($this->noperm);
+                  return true;
+				}else{
+					$sender->sendMessage(C::GREEN . "List of commands:" . C::WHITE . "\n- help(list helps)\n- annouce(sends a public message)");
+					return true;
+				}
 			}
+			#annouce cmd
+				if(count($args) >= 1 && $args[0] === "announce"){
+					if(!$sender->hasPermission("advancedmod.announce")){
+						$sender->sendMessage($this->noperm);
+                  return true;
+					}else{
+						
+						foreach($this->getServer()->getOnlinePlayers() as $p){
+							array_shift($args);
+								$txt = implode(" ", $args);
+								$p->sendMessage(C::RED . "[administrator] > " . C::YELLOW . $txt);
+							
+						}
+					
+					}
+				}
+			}
+//}
+			
 				
 			break;
 			break;
